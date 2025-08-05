39bffac33a7cecc7c26588e487aa9bc544f9c726
Yashaswi Prashar <136817480+Yashaswi-prashar@users.noreply.github.com>
2025-08-05 20:28:17 +0000

    Learning JavaScript datatypes and variable declarations

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553;
+let accountEmail = "yashaswiparashar24@gmail.com";
+var accountpassword = "1234";
+let accountcity = "Delhi";
+let accountstate;
+
+accountEmail = "yashiparashar24@gmail.com";
+accountpassword = "1236";
+accountcity = "Punjab";
+
+// accountId = 2; // ❌ This would throw an error
+
+console.log(accountId);
+console.log([accountId, accountEmail, accountpassword,accountstate]);
+
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version.
+
+// alert(3+3) //we are using nodejs ,not browser
+
+console.log(3+3);
+console.log("Yashi");
+
+// code redeability should be high 
+
+
+let name= "yashi"
+let age = 20
+let isLoggedIn = false
+console.log(typeof "Yashi");
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null=> standalone value
+// undefined =>
+// symbol => to find the uniquesness
+
+
+//object 
+
