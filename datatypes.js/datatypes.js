d904efe2a45773ff5f4eea002c0620f52536c3ab
Yashaswi Prashar <136817480+Yashaswi-prashar@users.noreply.github.com>
2025-08-06 09:45:34 +0000

    Summary of Datatypes

---
+++ datatypes.js/datatypes.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 types:  String, Number,  Boolean, null, Undefined, Symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn= false
+const outsidetemp= null
+
+const id = Symbol('575')
+const anotherId = Symbol('123')
+console.log(id==anotherId);
+
+
+// Referance (non primitive)
+
+// Array, Objects,Functions
+
+const heros = ["shaktiman", "iron mam"];
+letmyObj = {
+
+name : "yashi",
+age : 20
+}
+
+const myfucntion=function (){
+    console.log("hello world"); 
+    
+}
+
+
+
+
+
+
