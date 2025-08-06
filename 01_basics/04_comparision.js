a8d081dc6bd4c0e349b7abda248430ac4e58fa95
Yashaswi Prashar <136817480+Yashaswi-prashar@users.noreply.github.com>
2025-08-06 08:47:46 +0000

    Added JavaScript comparison examples

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+//console.log(2>1);
+//console.log(2>=1);
+
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null >=0);
+
+//Yhr reason is that an equality check == and comparision > < >= <= work differently.
+//Comparision convert null to a number , treat it as 0.
+//That's why (3) mull>0 is true add (1 ),(2)is false.
+
+// ===
+
+console.log("2"===2);
+
+
+
+
